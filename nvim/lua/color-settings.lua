f1d7e9d9be6fa0b4d04eeffcc717d50257b208c9
pavanbhat1999 <prbhat07@gmail.com>
2022-01-19 10:44:11 +0530

    rebased

--- nvim/lua/color-settings.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.termguicolors = true
-vim.g.background = 'dark'
-vim.cmd('source $HOME/.config/nvim/vim-scripts/color-settings.vim')
--- nvim/lua/non-setup.lua
+++
@@ -1,3 +0,0 @@
-require("neoscroll").setup()
-require('Comment').setup()
-require("telescope").load_extension('harpoon')
